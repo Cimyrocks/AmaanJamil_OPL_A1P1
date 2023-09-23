f7f09279975885771298b1b074ac2d794d5248a9
Amaan Jamil <80788403+Cimyrocks@users.noreply.github.com>
2023-09-22 21:40:45 -0400

    added readme and comments

--- bottles.pl
+++ bottles.pl
@@ -1,16 +1,23 @@
+%% function no bottles
 bottles(0):- write('no more bottles of beer').
+%% function for non-zero number of bottles
 bottles(B) :-
+    %% Write the first half of the stanza
     dif(B,0), NewB is B - 1,
     (
         dif(B,1), writef('%t bottles of beer on the wall \n',[B]), writef('%t bottles of beer\n',[B]);
         writef('%t bottle of beer on the wall \n',[B]), writef('%t bottle of beer\n',[B])
     ),
     write('Take one down, pass it around\n'),
+    %% Write the second half of the stanza with B - 1
     (
+        %% If it's greater than one than 
         NewB > 1, writef('%t bottles of beer on the wall \n\n',[NewB]);
+        %% If it's 1 then have it print the singular
         NewB =:= 1, writef('%t bottle of beer on the wall \n\n',[NewB]); 
         true
-    ), 
-    bottles(NewB).
-
+    ),
+    %% Call the bottles with B - 1
+    bottles(NewB). 
+%% Start from 99 bottles
 :- bottles(99).
---
+++ readme.txt
@@ -0,0 +1,36 @@
+I did this assignment on my own
+I wrote the Python and the OCaml code
+The other code was taken from rosetta code
+
+
+--------
+Prolog Instructions
+
+Use this command 
+swipl bottles.pl
+
+------------
+Go Instructions
+
+Use this command
+go run bottles.go
+
+-----------
+Python Instructions
+
+Use this command
+python3 bottles.py
+
+----------
+OCaml Instructions
+
+Use this command
+ocaml bottles.ml
+----------
+Ada Instructions
+Use this command to compile the code
+
+gnatmake bottles.adb
+
+then run the executable
+./bottles
